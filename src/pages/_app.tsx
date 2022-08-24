d1f325929289fd3aa02c12bacfbbd91777270ab1
Lucas <lucaszaquila@gmail.com>
2022-08-24 13:56:36 -0300

    Global style configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { AppProps } from "next/app";
+import '../styles/global.scss';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
