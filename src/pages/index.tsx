fd83a6cf79e95c453d0a2ddbbf5223a35d4be8be
Lucas <lucaszaquila@gmail.com>
2022-08-24 14:30:12 -0300

    Index filled with components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "../components/Footer";
+import Main from "../components/Main";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <h1>Init</h1>
-  )
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
 }
