200518bb3e92344ad4a63bf9d004f244e2f107f5
Lucas <lucaszaquila@gmail.com>
2022-08-26 17:47:39 -0300

    Fixing redux behaviour 	modified:   src/store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { cart } from './slice/cart'
 import { products } from './slice/products'
 
 export const store = configureStore({
   reducer: {
-    products
+    products,
+    cart
   },
 })
